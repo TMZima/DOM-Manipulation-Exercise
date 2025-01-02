519beda4663dc9288def40ba9197b1224fc50f00
Trey Zima <trey.m.zima@gmail.com>
2025-01-02 16:57:28 -0700

    Completed task 1

---
+++ solution.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  solveTask1();
+});
+
+function solveTask1() {
+  document.getElementById("task1").innerText = "Changed using 'innerText'.";
+}
