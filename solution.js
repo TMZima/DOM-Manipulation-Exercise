8d1ee2730ef26d4c7e198cc8b0fe67c579f18f4a
Trey Zima <trey.m.zima@gmail.com>
2025-01-02 17:02:16 -0700

    Completed task 2

--- solution.js
+++ solution.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   solveTask1();
+  solveTask2();
 });
 
 function solveTask1() {
   document.getElementById("task1").innerText = "Changed using 'innerText'.";
+}
+
+function solveTask2() {
+  document.getElementById("task2").innerHTML = "<button>Submit</button>";
 }
