cb0ff2f239780d5c27eb6f7bee98886da8d536e2
Trey Zima <trey.m.zima@gmail.com>
2025-01-02 17:09:04 -0700

    Completed task 3

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   solveTask1();
   solveTask2();
+  solveTask3();
+  solveTask4();
+  solveTask5();
+  solveTask6();
+  solveTask7();
+  solveTask8();
+  solveTask9();
 });
 
 function solveTask1() {
@@ -10,3 +17,19 @@ function solveTask1() {
 function solveTask2() {
   document.getElementById("task2").innerHTML = "<button>Submit</button>";
 }
+
+function solveTask3() {
+  document.body.style.backgroundColor = "#232323";
+}
+
+function solveTask4() {}
+
+function solveTask5() {}
+
+function solveTask6() {}
+
+function solveTask7() {}
+
+function solveTask8() {}
+
+function solveTask9() {}
